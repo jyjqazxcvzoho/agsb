4437b069b1274ec711315f4d34a418c58a6bcce1
jyjqazxcvzoho <jyjqazxcv@zohomail.com>
2025-06-06 18:38:42 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "jyj333"         # 用户名
+UUID = "35b0d353-fdd2-40cd-a772-823476bc2711"                     # UUID，留空则自动生成
+PORT = 38112                   # Vmess端口，留空或0则自动生成
+DOMAIN = "free.jyjqazxcv.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMGNlMjY1MzgwNTU4ZjBlZWVhMzZjNTkwMDFiYjliNGYiLCJ0IjoiZDJjY2JmYmMtNjcxYS00ZDljLTk4ZTAtNjZjNjIyMDA4MzViIiwicyI6IllXTTRNVEF3WkRZdE9XWTBNaTAwTURWaExUZ3paRFl0Wm1ZNFkyWTVOMk16WldabCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
